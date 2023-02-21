17bde1d835ab3789578a46d6098581c20e0ade2a
Conrad Schmidt <conrad@owatz.de>
2023-02-21 12:24:52 +0100

    www redirect

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import router from "./router";
 import "./assets/main.css";
 import {SneakApi} from "@/api";
 
+
+// redirect if www
+if (document.location.hostname.startsWith("www.")) {
+    document.location.hostname = document.location.hostname.replace("www.", "");
+}
+
 const app = createApp(App);
 app.use(router);
 app.config.globalProperties.$api = SneakApi;
